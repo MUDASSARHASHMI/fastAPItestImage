b29284b53ef65169b69ecb2351c49b9ef8a869fd
MUDASSARHASHMI <mudassarziahashmim@hotmail.com>
2024-11-11 07:11:03 +0500

    n

--- app/crud/base.py
+++ app/crud/base.py
@@ -47,3 +47,65 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
                 query = query.where(getattr(self.model, key) == value)
         result = await db.execute(query)
         return result.scalar_one_or_none()
+
+    # Search a specific entity
+    async def get_by_params(self, db: AsyncSession, **params: Any) -> Optional[ModelType]:
+        query = select(self.model)
+        for key, value in params.items():
+            if isinstance(value, str):
+                query = query.where(func.lower(getattr(self.model, key)) == func.lower(value))
+            else:
+                query = query.where(getattr(self.model, key) == value)
+        result = await db.execute(query)
+        return result.scalar_one_or_none()
+
+    # Add the entity
+    async def get_or_create(self, db: AsyncSession, defaults: Optional[Dict[str: Any]], **kwargs: Any):
+        instance = await self.get_by_params(db, **kwargs)
+        if instance:
+            return instance, False
+        params = defaults or {}
+        params.update(kwargs)
+        instance = self.model(**params)
+        db.add(instance)
+        await db.commit()
+        await db.refresh(instance)
+        return instance, True
+
+    # Partially update an entity
+    async def patch(self, db: AsyncSession, *, obj_id: str, obj_in: UpdateSchemaType | Dict[str, Any]) -> Optional[ModelType]:
+        db_obj = await self.get(db=db, obj_id=obj_id)
+        if not db_obj:
+            return None
+        update_data = obj_in if isinstance(obj_in, dict) else obj_in.model_dump(exclude_unset=True)
+        query = (
+            update(self.model).where(self.model.id == obj_id).values(**update_data)
+        )
+        await db.execute(query)
+        return await self.get(db, obj_id)
+    # Fully update and entity
+    async def update(self, db: AsyncSession, *, obj_current: ModelType,
+                     obj_new: UpdateSchemaType | Dict[str, Any] | ModelType):
+        obj_data = jsonable_encoder(obj_current)
+        if isinstance(obj_new, dict):
+            update_data = obj_new
+        else:
+            update_data = obj_new.model_dump(exclude_unset=True)
+        for field in update_data:
+            if field in update_data:
+                setattr(obj_current, field, update_data[field])
+        db.add(obj_current)
+        await db.commit()
+        await db.refresh(obj_current)
+        return obj_current
+
+    #Fully delete an entity from DB
+    async def remove(self, db: AsyncSession, *, obj_id: str):
+        db_obj = await self.get(db, obj_id)
+        if not db_obj:
+            return None
+        await db.delete()
+        await db.commit()
+
+        return db_obj
+
