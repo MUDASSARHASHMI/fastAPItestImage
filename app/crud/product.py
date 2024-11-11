1efacf797cfcff5cf0635a572668d2ca36f5a3ad
MUDASSARHASHMI <mudassarziahashmim@hotmail.com>
2024-11-11 07:28:59 +0500

    product Crud

--- app/crud/product.py
+++ app/crud/product.py
@@ -1,2 +1,24 @@
 from typing import Any, Optional, Coroutine, Dict
-from fastapi_pagination
+from fastapi_pagination import Page
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.util import await_only
+
+from .base import CRUDBase
+from app.schema.product import ProductUpdate, ProductCreate
+from app.models.products import Product
+
+class CRUDProduct(CRUDBase[Product, ProductCreate, ProductUpdate]):
+    async def get(self, db:AsyncSession, obj_id: str) -> Product:
+        return await super().get(db, obj_id)
+
+    async def get_or_create(self, db:AsyncSession, defaults: Dict[str, Any] | None, **kwargs: Any) -> Product:
+        return await super().get_or_create(db, defaults, **kwargs)
+
+    async def get_multi(self, db: AsyncSession, *, skip: int = 0, limit: int=20) -> Page[Product]:
+        return await super().get_multi(db, skip=skip, limit=limit)
+
+    async def update(self, db: AsyncSession, *, obj_current: Product, obj_new: ProductUpdate | Dict[str, Any] | Product):
+        return await super().update(db, obj_current=obj_current, obj_new=obj_new)
+    async def remove(self, db: AsyncSession, *, obj_id: str) -> Product | None:
+        return await super().remove(db, obj_id=obj_id)
+
